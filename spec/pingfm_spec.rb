39e3acdbceb81b8ae09498ea5ed675b0122f3db3
Kevin Williams <kevwil@gmail.com>
2008-08-29 16:35:08 -0600

    add some preliminary specs

--- spec/pingfm_spec.rb
+++ spec/pingfm_spec.rb
@@ -2,7 +2,34 @@
 
 require File.join(File.dirname(__FILE__), %w[spec_helper])
 
-describe Pingfm do
+describe Pingfm::Client, " with expected results" do
+  
+  before(:each) do
+    @client = Pingfm::Client.new('a','b')
+  end
+  
+  it "should validate keys successfully" do
+    # mock the http call
+    #Net::HTTP.should_receive(:post_form).with("#{Pingfm::API_URL}/user.validate",params).and_return(response)
+    
+  end
+  
+  it "should list the user's services properly" do
+    
+  end
+  
+  it "should list the user's custom triggers" do
+    
+  end
+  
+  it "should list the user's recent posts" do
+    
+  end
+  
+  it "should post a message to the service" do
+    
+  end
+  
 end
 
 # EOF
